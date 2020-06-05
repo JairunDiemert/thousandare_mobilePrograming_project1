2645c3027587e229350356c54bc9a893a6e4e53a
jsd25@students.uwf.edu <jsd25@students.uwf.edu>
2020-06-05 14:25:19 -0500

    Questions updating from game (view model)

--- Thousandaire/app/src/main/java/com/example/thousandaire/MainActivity.kt
+++ Thousandaire/app/src/main/java/com/example/thousandaire/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.thousandaire
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProviders
+import com.example.thousandaire.models.Game
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var questionTextView: TextView
+
+    private val game: Game by lazy {
+        ViewModelProviders.of(this).get(Game::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        questionTextView = findViewById(R.id.question_text_view)
+
+        updateQuestion()
     }
+
+    private fun updateQuestion() {
+        val questionTextResId = game.currentQuestionText
+        questionTextView.setText(questionTextResId)
+    }
+
 }
--- Thousandaire/app/src/main/java/com/example/thousandaire/models/Game.kt
+++ Thousandaire/app/src/main/java/com/example/thousandaire/models/Game.kt
@@ -1,6 +1,64 @@
 package com.example.thousandaire.models
 
 import androidx.lifecycle.ViewModel
+import com.example.thousandaire.R
 
 class Game : ViewModel() {
+
+    private val mickeyChoices = listOf<Int> (
+        R.string.answer_mickey_pluto,
+        R.string.answer_mickey_goofy,
+        R.string.answer_mickey_minnie,
+        R.string.answer_mickey_daisy
+    )
+
+    private val planetChoices = listOf<Int> (
+        R.string.answer_planet_earth,
+        R.string.answer_planet_mars,
+        R.string.answer_planet_jupiter,
+        R.string.answer_planet_venus
+    )
+
+    private val gilliganChoices = listOf<Int> (
+        R.string.answer_gilligan_2,
+        R.string.answer_gilligan_6,
+        R.string.answer_gilligan_7,
+        R.string.answer_gilligan_8
+    )
+
+    private val periodicChoices = listOf<Int> (
+        R.string.answer_periodic_Tc,
+        R.string.answer_periodic_O,
+        R.string.answer_periodic_Fe,
+        R.string.answer_periodic_E
+    )
+
+    private val vallettaChoices = listOf<Int> (
+        R.string.answer_valletta_croatia,
+        R.string.answer_valletta_latvia,
+        R.string.answer_valletta_estonia,
+        R.string.answer_valletta_malta
+    )
+
+    private val milesChoices = listOf<Int> (
+        R.string.answer_miles_1,
+        R.string.answer_miles_10,
+        R.string.answer_miles_100,
+        R.string.answer_miles_200
+    )
+
+    private val questionBank = listOf(
+        Question(R.string.question_mickey, R.string.answer_mickey_pluto, mickeyChoices, R.string.point_mickey_amount),
+        Question(R.string.question_planet, R.string.answer_planet_jupiter, planetChoices, R.string.point_planet_amount),
+        Question(R.string.question_gilligan, R.string.answer_gilligan_7, gilliganChoices, R.string.point_gilligan_amount),
+        Question(R.string.question_periodic, R.string.answer_periodic_E, periodicChoices, R.string.point_periodic_amount),
+        Question(R.string.question_valletta, R.string.answer_valletta_malta, vallettaChoices, R.string.point_valletta_amount),
+        Question(R.string.question_miles, R.string.answer_miles_100, milesChoices, R.string.point_miles_amount)
+    )
+
+    var currentIndex = 0
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].questionTextId
+
 }
